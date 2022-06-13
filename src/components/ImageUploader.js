5872691e923a1fd860f90613305606950064afce
Djibril Bathily <bathilydbl@gmail.com>
2022-06-13 15:59:16 +0200

    handle drag and drop

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,9 +1,29 @@
+import { useEffect, useRef } from "react";
 import imagePlaceholder from "../assets/image.svg";
 
 function ImageUploader() {
+  const dropArea = useRef();
+
+  useEffect(() => {
+    dropArea.current.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      dropArea.current.classList.add("active");
+    });
+
+    dropArea.current.addEventListener("dragleave", () => {
+      dropArea.current.classList.remove("active");
+    });
+
+    dropArea.current.addEventListener("drop", (e) => {
+      e.preventDefault();
+      dropArea.current.classList.remove("active");
+      console.log(e.dataTransfer.files[0]);
+    });
+  }, []);
+
   return (
     <>
-      <div className="drag-area">
+      <div className="drag-area" ref={dropArea}>
         <img src={imagePlaceholder} />
         <p>Drag & Drop your image here</p>
         <input type="file" hidden></input>
